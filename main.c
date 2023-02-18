2b71732681f38ce1c9f6c9cb2ac184e73ff7445b
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-02-18 20:53:19 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <math.h> 
+#include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
+
 #define _USE_MATH_DEFINES
 #define N 10000000
-# define M_PI 3.14159265358979323846 // pi
+#define M_PI 3.14159265358979323846 // pi
 
+double Sum=0;
 double arr[N];
 
 int main()
 {
-	for (int i = 0; i < 100; i++)
+	// для хранения времени выполнения кода
+	double time_spent = 0.0;
+
+	clock_t begin = clock();
+
+#pragma acc kernels
+	for (int i = 0; i < N; i++)
 	{
-		arr[i] = sin((i + 1) * M_PI / 180);
+		arr[i] = sin(i * M_PI *2 / N);
+		Sum += arr[i];
 	}
 	
+	clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+
+	printf("%f\n", time_spent);
+	printf("%0.25f", Sum);
+
 	return 0;
 }
