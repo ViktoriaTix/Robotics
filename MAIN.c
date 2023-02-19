43db1ead3d88972d0a648bb76fc5fae91aed9810
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-02-19 22:39:55 +0700

    Update MAIN.c

--- MAIN.c
+++ MAIN.c
@@ -5,31 +5,40 @@
 #define _USE_MATH_DEFINES
 #define N 10000000
 #define M_PI 3.14159265358979323846 // pi
-
-float Sum = 0;
+	
+float Sum=0;
 float arr[N];
 
 int main()
 {
-	// для хранения времени выполнения кода
 	double time_spent = 0.0;
 
-	clock_t begin = clock();
+	clock_t begin1 = clock();
 
 #pragma acc kernels
 	for (int i = 0; i < N; i++)
 	{
-		arr[i] = (float)sin(i * M_PI * 2 / N);
+		arr[i] = sin(i * M_PI *2 / N);
 	}
 
-	clock_t end = clock();
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	clock_t end1 = clock();
+	time_spent1 += (double)(end1 - begin1) / CLOCKS_PER_SEC;
 
+	double time_spent = 0.0;
+	
+	clock_t begin2 = clock();
+	
+#pragma acc kernels	
 	for (int i = 0; i < N; i++)
 	{
 		Sum += arr[i];
 	}
-	printf("%f\n", time_spent);
+	
+	clock_t end2 = clock();
+	time_spent2 += (double)(end2 - begin2) / CLOCKS_PER_SEC;
+	
+	printf("%f\n", time_spent1);
+	printf("%f\n", time_spent2);
 	printf("%0.25f\n", Sum);
 
 	return 0;
