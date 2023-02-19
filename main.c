4ac134b44eda2a09c4ceb0323d9b11392cdb3162
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-02-19 22:38:10 +0700

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main()
 {
 	double time_spent = 0.0;
 
-	clock_t begin = clock();
+	clock_t begin1 = clock();
 
 #pragma acc kernels
 	for (int i = 0; i < N; i++)
@@ -21,15 +21,24 @@ int main()
 		arr[i] = sin(i * M_PI *2 / N);
 	}
 
-	clock_t end = clock();
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	clock_t end1 = clock();
+	time_spent1 += (double)(end1 - begin1) / CLOCKS_PER_SEC;
+
+	double time_spent = 0.0;
 	
+	clock_t begin2 = clock();
+	
+#pragma acc kernels	
 	for (int i = 0; i < N; i++)
 	{
 		Sum += arr[i];
 	}
 	
-	printf("%f\n", time_spent);
+	clock_t end2 = clock();
+	time_spent2 += (double)(end2 - begin2) / CLOCKS_PER_SEC;
+	
+	printf("%f\n", time_spent1);
+	printf("%f\n", time_spent2);
 	printf("%0.25f\n", Sum);
 
 	return 0;
