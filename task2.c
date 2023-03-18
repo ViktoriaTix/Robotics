91d7c6f1d9a3bc80bd0985f24e21bae0f04bf6f6
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-03-19 00:16:19 +0700

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,27 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <stdio.h>
 
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 void heat_equation_2d(int n, double k, int max_iters, double tol, int* iters, double* err)
 {
     // Allocate memory for the grid with ghost cells
-    double** u = (double**)malloc((n + 2) * sizeof(double*));
-    for (int i = 0; i < n + 2; i++) {
-        u[i] = (double*)malloc((n + 2) * sizeof(double));
-    }
+    double* u = (double*)malloc((n + 2) * (n + 2) * sizeof(double));
     // Initialize the grid with the boundary conditions
-    u[0][0] = 10;
-    u[0][n + 1] = 20;
-    u[n + 1][0] = 30;
-    u[n + 1][n + 1] = 20;
+    u[0*(n+2)+0] = 10;
+    u[0*(n+2)+(n+1)] = 20;
+    u[(n+1)*(n+2)+0] = 30;
+    u[(n+1)*(n+2)+(n+1)] = 20;
     for (int i = 1; i <= n; i++) {
-        u[0][i] = 10 + i * (20 - 10) / (double)(n + 1);
-        u[n + 1][i] = 20 + i * (30 - 20) / (double)(n + 1);
-        u[i][0] = 10 + i * (30 - 10) / (double)(n + 1);
-        u[i][n + 1] = 30 + i * (20 - 30) / (double)(n + 1);
+        u[0*(n+2)+i] = 10 + i * (20 - 10) / (double)(n + 1);
+        u[(n+1)*(n+2)+i] = 20 + i * (30 - 20) / (double)(n + 1);
+        u[i*(n+2)+0] = 10 + i * (30 - 10) / (double)(n + 1);
+        u[i*(n+2)+(n+1)] = 30 + i * (20 - 30) / (double)(n + 1);
     }
     // Calculate the grid spacing
     double h = 1 / (double)(n + 1);
@@ -30,48 +26,45 @@ void heat_equation_2d(int n, double k, int max_iters, double tol, int* iters, do
     *iters = 0;
     // Perform the iterative updates
     while (err_max > tol && *iters < max_iters) {
-        double** u_new = (double**)malloc((n + 2) * sizeof(double*));
-        for (int i = 0; i < n + 2; i++) {
-            u_new[i] = (double*)malloc((n + 2) * sizeof(double));
-        }
+        double* u_new = (double*)malloc((n + 2) * (n + 2) * sizeof(double));
 #pragma acc parallel loop
         for (int i = 1; i <= n; i++) {
 #pragma acc loop
             for (int j = 1; j <= n; j++) {
-                u_new[i][j] = (1 / 4.0) * (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) - (k * h * h / 4.0) * (u[i][j + 1] + u[i][j - 1] + u[i - 1][j] + u[i + 1][j] - 4 * u[i][j]);
+                double* u_ij = &u[i*(n+2)+j];
+                double* u_new_ij = &u_new[i*(n+2)+j];
+                double* u_im1j = &u[(i-1)*(n+2)+j];
+                double* u_ip1j = &u[(i+1)*(n+2)+j];
+                double* u_ijm1 = &u[i*(n+2)+j-1];
+                double* u_ijp1 = &u[i*(n+2)+j+1];
+                *u_new_ij = (1 / 4.0) * (*u_im1j + *u_ip1j + *u_ijm1 + *u_ijp1)
+                    - (k * h * h / 4.0) * (*u_ijp1 + *u_ijm1 + *u_im1j + *u_ip1j - 4 * (*u_ij));
             }
         }
-        // Calculate the maximum error
+       // Calculate the maximum error
         err_max = 0.0;
 #pragma acc parallel loop reduction(max:err_max)
         for (int i = 1; i <= n; i++) {
-#pragma acc loop reduction(max:err_max)
             for (int j = 1; j <= n; j++) {
-                err_max = fmax(err_max, fabs(u_new[i][j] - u[i][j]));
+                 double* u_ij = &u[i*(n+2)+j];
+                double* u_new_ij = &u_new[i*(n+2)+j];
+                err_max = fmax(err_max, fabs(*u_new_ij - *u_ij));
             }
         }
-        // Update the grid
-#pragma acc parallel loop
+        // Copy the updated grid to the old grid and free the memory for the new grid
+#pragma acc parallel loop collapse(2)
         for (int i = 1; i <= n; i++) {
-#pragma acc loop
             for (int j = 1; j <= n; j++) {
-                u[i][j] = u_new[i][j];
+                u[i*(n+2)+j] = u_new[i*(n+2)+j];
             }
-        }
-        // Free memory for the new grid
-        for (int i = 0; i < n + 2; i++) {
-            free(u_new[i]);
         }
         free(u_new);
-        // Increment the iteration counter
-        (*iters)++;
-        // Set the error
+
+        // Update the iteration counter and error
+        *iters += 1;
         *err = err_max;
     }
-    // Free memory for the grid
-    for (int i = 0; i < n + 2; i++) {
-        free(u[i]);
-    }
+    // Free the memory for the grid
     free(u);
 }
 
