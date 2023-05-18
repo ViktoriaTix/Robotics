5652b1694d7f81a7eb63337481382de1234fcdbc
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-05-18 13:12:20 +0700

    Update task4.cu

--- task4.cu
+++ task4.cu
@@ -6,6 +6,32 @@
 #include <time.h>
 
 
+// функция изменения матрицы уравнения теплопроводности
+__global__ void calculate(double *CudaArr, double *CudaNewArr, size_t Matrix)
+{
+    size_t i = blockIdx.x + 1;
+    size_t j = threadIdx.x + 1;
+    int index = i * Matrix + j;
+    CudaNewArr[index] = 0.25 * (CudaArr[(i - 1) * Matrix + j] + CudaArr[(i + 1) * Matrix + j] + CudaArr[index - 1] + CudaArr[index + 1]);
+}
+
+
+// функция разницы матриц
+__global__ void subtraction(double* CudaArr, double* CudaNewArr)
+{
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	CudaNewArr[idx] = CudaArr[idx] - CudaNewArr[idx];
+}
+
+// Функция востановления границ матрицы
+__global__ void restore(double* A, int size){
+	size_t i = threadIdx.x;
+	A[i] = 10.0 + i * 10.0 / (size - 1);
+	A[i * size] = 10.0 + i * 10.0 / (size - 1);
+	A[size - 1 + i * size] = 20.0 + i * 10.0 / (size - 1);
+	A[size * (size - 1) + i] = 20.0 + i * 10.0 / (size - 1);
+}
+
 int main(int argc, char* argv[]) {
     
     double time_spent = 0.0;
@@ -16,59 +42,25 @@ int main(int argc, char* argv[]) {
     int Matrix = atoi(argv[1]);
     double accuracy = atof(argv[2]);
     int iterations = atoi(argv[3]);
-    
-    // Allocate 1D arrays on host memory
-    double* arr = (double*)malloc(Matrix * Matrix * sizeof(double));
-    double* arr_new = (double*)malloc(Matrix * Matrix * sizeof(double));
 
-    // Initialize arrays to zero
-    for (int i = 0; i < Matrix * Matrix; i++) {
-        arr[i] = 0;
-        arr_new[i] = 0;
-    }
-    
-    // Set boundary conditions
-    arr[0] = 10;
-    arr[Matrix - 1] = 20;
-    arr[(Matrix - 1) * Matrix] = 30;
-    arr[Matrix * Matrix - 1] = 20;
-
-    arr_new[0] = 10;
-    arr_new[Matrix - 1] = 20;
-    arr_new[(Matrix - 1) * Matrix] = 30;
-    arr_new[Matrix * Matrix - 1] = 20;
- 
-    for(int i = 1; i < Matrix - 1; i++)
-    {
-        arr[i*Matrix] = arr[(i-1)*Matrix]+ (10 / (Matrix - 1));
-        arr[i] = arr[i-1] + (10 / (Matrix - 1));
-        arr[(Matrix-1)*Matrix + i] = arr[(Matrix-1)*Matrix + i-1] + (10 / (Matrix - 1));
-        arr[i*Matrix + (Matrix-1)] = arr[(i-1)*Matrix + (Matrix-1)] + (10 / (Matrix - 1));
-
-        arr_new[i*Matrix] = arr_new[(i-1)*Matrix]+ (10 / (Matrix - 1));
-        arr_new[i] = arr_new[i-1] + (10 / (Matrix - 1));
-        arr_new[(Matrix-1)*Matrix + i] = arr_new[(Matrix-1)*Matrix + i-1] + (10 / (Matrix - 1));
-        arr_new[i*Matrix + (Matrix-1)] = arr_new[(i-1)*Matrix + (Matrix-1)] + (10 / (Matrix - 1));
-    }
-
+    cudaSetDevice(1);
     
     // создание потока
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
+	bool graphCreated = false;
     // создание графа
     cudaGraph_t graph;
     cudaGraphExec_t graph_exec;
 
     // выделяем память на gpu через cuda для 3 сеток
-    double *CudaArr, *CudaNewArr, *CudaDifArr;
+    double *CudaArr, *CudaNewArr;
     cudaMalloc((void **)&CudaArr, sizeof(double) * Matrix * Matrix);
     cudaMalloc((void **)&CudaNewArr, sizeof(double) * Matrix * Matrix);
-    cudaMalloc((void **)&CudaDifArr, sizeof(double) * Matrix * Matrix);
 
-    // копирование информации с CPU на GPU
-    cudaMemcpy(CudaArr, arr, sizeof(double) * Matrix * Matrix, cudaMemcpyHostToDevice);
-    cudaMemcpy(CudaNewArr, arr_new, sizeof(double) * Matrix * Matrix, cudaMemcpyHostToDevice);
+    restore<<<1, Matrix>>>(CudaArr, Matrix);
+    cudaMemcpy(CudaNewArr, CudaArr, sizeof(double) * Matrix * Matrix, cudaMemcpyHostToDevice);
 
     // выделяем память на gpu. Хранение ошибки на device
     double *max_err = 0;
@@ -78,51 +70,48 @@ int main(int argc, char* argv[]) {
     double *tempStorage = NULL;
 
     // получаем размер временного буфера для редукции
-    cub::DeviceReduce::Max(tempStorage, tempStorageBytes, CudaDifArr, max_err, Matrix * Matrix, stream);
+    cub::DeviceReduce::Max(tempStorage, tempStorageBytes, CudaNewArr, max_err, Matrix * Matrix, stream);
 
     // выделяем память для буфера
-    cudaMalloc((void **)&tempStorage, tempStorageBytes);
-
-    cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
-
-    // Compute new values
-    for (int j = 1; j < Matrix - 1; j++) {
-        for (int i = 1; i < Matrix - 1; i++) {
-            int index = j * Matrix + i;
-            CudaNewArr[index] = 0.25 * (CudaArr[index + Matrix] + CudaArr[index - Matrix] +
-                CudaArr[index - 1] + CudaArr[index + 1]);
-            CudaDifArr[index] = fabs(CudaNewArr[index] - CudaArr[index]);
-        }
-    }
-    
-    // Compute maximum error using CUB
-    cub::DeviceReduce::Max(tempStorage, tempStorageBytes, CudaDifArr, max_err, Matrix * Matrix, stream);
-    
-
-    cudaStreamEndCapture(stream, &graph);
-    cudaGraphInstantiate(&graph_exec, graph, NULL, NULL, 0);
+    cudaMalloc(&tempStorage, tempStorageBytes);
 
     // Main loop
-    double err = accuracy + 1;
+    double err = 1;
     int iter = 0;
 
     while (err > accuracy && iter < iterations) {
-        
-        err = 0;
-        cudaGraphLaunch(graph_exec, stream);
-        // запись ошибки в переменную
-        cudaMemcpy(&err, max_err, sizeof(double), cudaMemcpyDeviceToHost);
-        err = std::abs(err);
 
-        // Update values
-        for (int j = 1; j < Matrix - 1; j++) {
-            for (int i = 1; i < Matrix - 1; i++) {
-                int index = j * Matrix + i;
-                arr[index] = arr_new[index];
+        if(!graphCreated){
+
+            //создаем граф
+            cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+
+            // Compute new values
+            for (size_t i = 0; i < 100; i += 2) {
+                calculate<<<Matrix - 2, Matrix-2, 0, stream>>>(CudaArr, CudaNewArr, Matrix);
+                calculate<<<Matrix-2, Matrix-2, 0, stream>>>(CudaNewArr, CudaArr, Matrix);
             }
+            subtraction<<<Matrix, Matrix, 0, stream>>>(CudaArr, CudaNewArr);
+
+            // Compute maximum error using CUB
+            cub::DeviceReduce::Max(tempStorage, tempStorageBytes, CudaNewArr, max_err, Matrix * Matrix, stream);
+            restore<<<1, Matrix, 0, stream>>>(CudaNewArr, Matrix);
+            //printf("hi");
+
+            cudaStreamEndCapture(stream, &graph);
+            cudaGraphInstantiate(&graph_exec, graph, NULL, NULL, 0);
+            graphCreated=true;
+
+        }
+        else{
+            cudaGraphLaunch(graph_exec, stream);
+            // запись ошибки в переменную
+            cudaMemcpyAsync(&err, max_err, sizeof(double), cudaMemcpyDeviceToHost, stream);
+			cudaStreamSynchronize(stream);
+            iter+=100;
+            graphCreated=false;
         }
         
-        iter++;
     }
 
     printf("Final result: %d, %0.6lf\n", iter, err);
@@ -131,13 +120,8 @@ int main(int argc, char* argv[]) {
     cudaStreamDestroy(stream);
     cudaGraphDestroy(graph);
 
-    // Free memory
-    free(arr);
-    free(arr_new);
-
     cudaFree(CudaArr);
     cudaFree(CudaNewArr);
-    cudaFree(CudaDifArr);
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
