e2c6c5f6fdc0c6a35d88f923136dd1940ebeecd5
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-09-14 16:59:26 +0700

    Add files via upload

--- lab2/ex03/my_first_package/setup.py
+++ lab2/ex03/my_first_package/setup.py
@@ -1,1 +1,26 @@
+from setuptools import find_packages, setup
 
+package_name = 'my_first_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vikatop',
+    maintainer_email='v.tikhonova@g.nsu.ru',
+    description='Beginner client libraries tutorials practice package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_first_package.my_node:main'
+        ],
+    },
+)
