d6ced53cf2258b3f7e24f1e31fbaad40a2306077
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-10-11 01:57:54 +0700

    Update action_turtle_server.py

--- lab3/ex02/python_turtle_commands/python_turtle_commands/action_turtle_server.py
+++ lab3/ex02/python_turtle_commands/python_turtle_commands/action_turtle_server.py
@@ -1,78 +1,85 @@
-import rclpy
-from rclpy.action import ActionServer
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-from action_turtle_commands.action import MessageTurtleCommands
-import time
 import math
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from rclpy.action import ActionServer
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from action_turtle_commands.action import MessageTurtleCommands
+
 
 class CommandActionServer(Node):
 
     def __init__(self):
         super().__init__('action_turtle_server')
-        self.float_odom = 0.0
-        self.flag = 0
+        
         self.twist = Twist()
-        self.before_pose = Pose()
+        self.flag = 0
         self.after_pose = Pose()
+        self.before_pose = Pose()
+        
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.callback, 10)
         self._action_server = ActionServer(
             self,
             MessageTurtleCommands,
-            'move_turtle',
+            'execute_turtle_commands',
             self.execute_callback)
-        self.goal_handle = None
+        
+        self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.callback, 10)
         self.subscription 
 
     def callback(self, msg):
+        self.after_pose = msg
         if self.flag == 1:
             self.before_pose = msg
             self.flag = 0
-        self.after_pose = msg
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
-
+        self.flag = 1
+        
         if goal_handle.request.command == 'forward':
             self.twist.linear.x = float(goal_handle.request.s)
             self.twist.angular.z = 0.0
+            
         elif goal_handle.request.command == 'turn_left':
             self.twist.linear.x = 0.0
-            self.twist.angular.z = float(goal_handle.request.angle*3.14/180)
+            self.twist.angular.z = float(goal_handle.request.angle) * math.pi /180
+            
         elif goal_handle.request.command == 'turn_right':
             self.twist.linear.x = 0.0
-            self.twist.angular.z = -float(goal_handle.request.angle*3.14/180)
-
-        self.flag = 1
+            self.twist.angular.z = -float(goal_handle.request.angle) * math.pi /180
+            
         self.publisher_.publish(self.twist)
         
-        time.sleep(1)
-        
         feedback_msg = MessageTurtleCommands.Feedback()
-        feedback_msg.odom = 1
-        goal_handle.publish_feedback(feedback_msg)
+        feedback_msg.odom = 0
         
+        while self.flag == 1 and (self.after_pose.linear_velocity==0 or self.after_pose.angular_velocity==0):
+            pass
+            
+        while self.after_pose.linear_velocity != 0 or self.after_pose.angular_velocity != 0:
+            feedback_msg.odom = int(math.sqrt((self.after_pose.x - self.before_pose.x)**2+(self.after_pose.y - self.before_pose.y)**2))
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.odom))
+            goal_handle.publish_feedback(feedback_msg)
+            
         goal_handle.succeed()
-        self.goal_handle = goal_handle
-        return MessageTurtleCommands.Result(result=True)
+        result = MessageTurtleCommands.Result()
+        result.result = True
+        return result
 
-    def cancel_callback(self, goal_handle):
-        self.get_logger().info('Goal canceled')
-        self.publisher_.publish(Twist())
-        goal_handle.canceled()
-        self.flag = 0
 
 def main(args=None):
     rclpy.init(args=args)
     action_turtle_server = CommandActionServer()
-    try:
-        rclpy.spin(action_turtle_server)
-    finally:
-        action_turtle_server.destroy()
-        rclpy.shutdown()
+    executor = MultiThreadedExecutor(num_threads=2)
+    executor.add_node(action_turtle_server)
+    executor.spin()
+    executor.shutdown()
+    action_turtle_server.destroy_node()
+    
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
